05da7217b3ca368c1b0f15ccddb5661b1cfdd80e
Alexey Kutepov <alkutepov@gmail.com>
2019-07-18 19:10:07 +0300

    Урок 4 ДЗ

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -9,21 +9,37 @@ import ru.geekbrains.base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
 
+    private static final float V_LEN = 0.01f;
+
     private Texture img;
+    private Vector2 pos;
+    private Vector2 endPos;
+    private Vector2 v;
+    private Vector2 buf;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
+        pos = new Vector2();
+        endPos = new Vector2();
+        v = new Vector2();
+        buf = new Vector2();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
+        buf.set(endPos);
+        if (buf.sub(pos).len() > V_LEN) {
+            pos.add(v);
+        } else {
+            pos.set(endPos);
+        }
         Gdx.gl.glClearColor(0.26f, 0.5f, 0.8f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img, 0f, 0f, 0.15f, 0.15f);
+        batch.draw(img, pos.x, pos.y, 0.15f, 0.15f);
         batch.end();
     }
 
@@ -35,6 +51,8 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
+        endPos.set(touch);
+        v.set(touch.sub(pos)).setLength(V_LEN);
         return false;
     }
 }
